ed7bb88d52c7e6575c827be5ec8031de4d9da4d3
Caroline Abbott <cpabbott@uchicago.edu>
2019-09-10 15:04:27 -0400

    [WIP] a small change

--- lottery.R
+++ lottery.R
@@ -9,15 +9,15 @@
 
 library(stats)
 
-arr <- array(0,10)
+ten_coin_flips <- array(0,10)
 for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+  ten_coin_flips[i] <- if (runif(1) < 0.1) 1 else 0
 }
 
 for (j in 1:100){
-  arr <- array(0,10)
+  ten_coin_flips <- array(0,10)
   for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+    ten_coin_flips[i] <- if (runif(1) < 0.1) 1 else 0
   }
-  print(arr)
+  print(ten_coin_flips)
 }
