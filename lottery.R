35de2f95f1eb712906e71bec970dd54d45e3d898
Caroline Abbott <cpabbott@uchicago.edu>
2019-09-10 15:15:24 -0400

    lovely clean and annotated code

--- lottery.R
+++ lottery.R
@@ -9,15 +9,13 @@
 
 library(stats)
 
-ten_coin_flips <- array(0,10)
-for (i in 1:10){
-  ten_coin_flips[i] <- if (runif(1) < 0.1) 1 else 0
-}
-
-for (j in 1:100){
-  ten_coin_flips <- array(0,10)
-  for (i in 1:10){
-    ten_coin_flips[i] <- if (runif(1) < 0.1) 1 else 0
+run_coin_exp <- function(last_flip, tails, heads, p){ #function that runs 100 sets of 10 coin flips
+for (j in 1:last_flip){                               #for every time until last flip
+  ten_coin_flips <- array(0,10)                       #make 10 flips
+  for (i in 1:10){                                    #and for each of those flips
+    ten_coin_flips[i] <- if (runif(1) < p) tails else heads  #set a weighted prob for t/h
+    }
+  print(ten_coin_flips)                               #and then print them
   }
-  print(ten_coin_flips)
 }
+run_coin_exp(last_flip = 100, tails = 1, heads = 0, p = 0.1) #our variable parameters
